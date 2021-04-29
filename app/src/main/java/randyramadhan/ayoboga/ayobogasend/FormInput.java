e93db7a8232de07c3cc5454ab2e89eb79df5e0bd
randynetworks <randyramadhan1973@gmail.com>
2021-04-29 07:57:39 +0700

    initial Project + splashscreen

---
+++ app/src/main/java/randyramadhan/ayoboga/ayobogasend/FormInput.java
@@ -0,0 +1,15 @@
+package randyramadhan.ayoboga.ayobogasend;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormInput extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_input);
+    }
+}
---
+++ app/src/main/java/randyramadhan/ayoboga/ayobogasend/MainActivity.java
@@ -0,0 +1,28 @@
+package randyramadhan.ayoboga.ayobogasend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,FormInput.class);
+                MainActivity.this.startActivity(i);
+                MainActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
